794b954fd6f0f758b6200cb1bf26b5fbb7ad04e5
linxia25 <linxia25@hotmail.com>
2021-03-12 16:44:05 -0700

    Change the server.js file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Add more middleware
+// Use res.sendFile to send the 404.html page when the http status is 404
 app.get("*",function(req, res) {
   res.status(404);
   res.sendFile(__dirname + "/public/404.html");
